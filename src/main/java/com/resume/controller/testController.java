dd6aaaece3ee30940fc55965970753087ea8dec3
경제목 <jemok9605@naver.com>
2023-03-25 13:19:52 +0900

    Initial commit

---
+++ src/main/java/com/resume/controller/testController.java
@@ -0,0 +1,5 @@
+package com.resume.controller;
+
+
+public class testController {
+}
---
+++ src/test/java/com/resume/dto/MapperConnection.java
@@ -0,0 +1,26 @@
+package com.resume.dto;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Slf4j
+public class MapperConnection {
+    @Test
+    void dbConnectionTest() {
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://jmsj.cwgb1dyb6cj0.ap-northeast-2.rds.amazonaws.com:3306/jmsj?characterEncoding=UTF-8&serverTimezone=UTC",
+                    "admin",
+                    "gkdlfn12");
+            log.info("con={}", con);
+        }
+        catch (Exception e){
+            log.error("error ",e);
+        }
+
+    }
+}
