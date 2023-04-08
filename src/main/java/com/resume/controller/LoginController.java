2bb80ef19374b58b0d887f049f32ac522845f02d
경제목 <jemok9605@naver.com>
2023-04-09 02:30:37 +0900

    [feature]email로직 해킹 당하고 수정함

--- src/main/java/com/resume/controller/LoginController.java
+++ src/main/java/com/resume/controller/LoginController.java
@@ -1,34 +1,35 @@
 package com.resume.controller;
 
 
-import com.resume.config.MailService;
+
 import com.resume.dto.LoginUserDTO;
 import com.resume.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashSet;
-import java.util.Set;
 
 @Slf4j
 @Controller
 public class LoginController {
 
-
+    private final JavaMailSender javaMailSender;
     private final LoginService loginService;
 
-
-    public LoginController(LoginService loginService ) {
+    public LoginController(JavaMailSender javaMailSender, LoginService loginService) {
+        this.javaMailSender = javaMailSender;
         this.loginService = loginService;
+        log.info("login={}",loginService);
+        log.info("javaMailsender={}",javaMailSender);
 
     }
 
     @GetMapping("/login")
     public String login() {
-
         return "login/login";
     }
 
@@ -39,11 +40,31 @@ public class LoginController {
         log.info("password = {}", password);
         LoginUserDTO loginUserDTO = loginService.checkUser(userid);
         System.out.println("loginUserDTO = " + loginUserDTO);
-
         return "login/login";
 
     }
 
+    @GetMapping("/findid")
+    public String findId(){
+        return "login/findid";
+    }
+    @GetMapping("/checkid")
+    public String checkId(@RequestParam("email")String email){
+        mailSend(email);
+        return "login/findid";
+    }
+
+
+
+
+    private void mailSend(String email) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(email);
+        simpleMailMessage.setSubject("title");
+        simpleMailMessage.setText("Text Mail Sender");
+
+        javaMailSender.send(simpleMailMessage);
+    }
 
 
 
