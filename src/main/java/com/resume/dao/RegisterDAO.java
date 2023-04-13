53f612aa76e57f24d818ba88c4dbcbaedd62a016
SungJinJang96 <sjphone95@naver.com>
2023-04-13 23:56:59 +0900

    2023-04-14  - 메인 페이지 개인정보 첫페이지 뿌리기  - 로그아웃 마이페이지 버튼 추가

--- src/main/java/com/resume/dao/RegisterDAO.java
+++ src/main/java/com/resume/dao/RegisterDAO.java
@@ -4,6 +4,8 @@ import com.resume.dto.RegisterDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @Mapper
 public interface RegisterDAO {
@@ -11,4 +13,6 @@ public interface RegisterDAO {
     int saveuser(RegisterDTO user);
 
     int idcheck(String userid);
+
+    RegisterDTO userInfo(String sessionid);
 }
--- src/main/java/com/resume/service/RegisterService.java
+++ src/main/java/com/resume/service/RegisterService.java
@@ -5,6 +5,8 @@ import com.resume.dto.RegisterDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Slf4j
 public class RegisterService {
@@ -22,5 +24,9 @@ public class RegisterService {
 
     public int idcheck(String userid) {
         return dao.idcheck(userid);
+    }
+
+    public RegisterDTO userInfo(String sessionid) {
+        return dao.userInfo(sessionid);
     }
 }
