8b5ee44fb6280a815678b7485ca46321993766fd
경제목 <jemok9605@naver.com>
2023-06-30 18:55:12 +0900

    [feature] 공지사항 페이징 처리

--- src/main/java/com/resume/dao/NoticeDAO.java
+++ src/main/java/com/resume/dao/NoticeDAO.java
@@ -4,10 +4,15 @@ import com.resume.dto.NoticeDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 @Repository
 @Mapper
 public interface NoticeDAO {
     int saveNotice(NoticeDTO dto);
     int deleteNotice(int num);
     int selectNoticeOne(NoticeDTO dto);
+    List<NoticeDTO> findAll(Map map);
+    int noticeCount();
 }
--- src/test/java/com/resume/service/NoticeServiceTest.java
+++ src/test/java/com/resume/service/NoticeServiceTest.java
@@ -27,6 +27,21 @@ class NoticeServiceTest {
         int num = noticeService.selectNoticeOne(notice);
         log.info("num={}",num);
         Assertions.assertThat(noticeService.deleteNotice(num)==1);
+    }
+    @Test
+    void pagingCountTest() throws Exception{
+        //given
+        NoticeDTO[] dto = new NoticeDTO[110];
+        for(int i =0; i<dto.length; i++){
+            dto[i] = new NoticeDTO();
+            dto[i].setUserid("admin");
+            dto[i].setTitle("test"+i);
+            dto[i].setContents("testContents"+i);
+            noticeService.saveNotice(dto[i]);
+        }
+        //when
+
+        //then
 
     }
 }
