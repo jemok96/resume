49ab69198a195a7a8d45af4450bf49a7148693c0
경제목 <jemok9605@naver.com>
2023-07-10 21:24:48 +0900

    [test] jenkins 자동 build test

--- src/main/java/com/resume/controller/BoardController.java
+++ src/main/java/com/resume/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.resume.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoardController {
+    @RequestMapping("/jenkins/buildTest")
     public String mainForm(){
         return "login/login";
     }
