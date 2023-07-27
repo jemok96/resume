53826723a389416d74c3b2007b3d0d5b5d1bccb9
경제목 <jemok9605@naver.com>
2023-07-27 19:39:28 +0900

    [fix]이미지 변경 시 프로젝트에도 저장되는오류 경로 변경

--- src/test/java/com/resume/service/S3UploadServiceTest.java
+++ src/test/java/com/resume/service/S3UploadServiceTest.java
@@ -1,23 +1,22 @@
 package com.resume.service;
 
+import com.resume.dto.AwsS3Dto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
 class S3UploadServiceTest {
+
     @Autowired
-    private S3UploadService service;
-
-    @Test
-    void uploadTest() throws Exception{
-        String imgPath = service.getObjectUrl("upload/0710eb94-3d25-4c82-901d-62878dfed3e1KakaoTalk_20230423_201016500.jpg");
-        log.info("path={}",imgPath);
-
-    }
+    S3UploadService service;
 
 }
